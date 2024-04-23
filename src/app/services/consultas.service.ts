2ea58885bde206d5fb73ff8d26b8abac1dbe5182
Felipe Lelles Carneiro <contactfelipelc@gmail.com>
2024-04-23 14:47:41 -0300

    code cleaning

--- src/app/services/consultas.service.ts
+++ src/app/services/consultas.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +8,8 @@ export class ConsultasService {
 
   // apiKey twelvedata
   apiKey = '13a61d54da3947b6bdc7c92eb9747b8c'
+  
   ativos = 'EUR-BBD,EUR-BDT,EUR-BGN,EUR-BHD,EUR-BIF,EUR-BND,BTC-USD,ETH-USD,LTC-USD,XRP-USD,DOGE-USD,GBP-USD,EUR-CHF,EUR-USD,EUR-CAD,EUR-JPY,EUR-NZD,EUR-AUD,EUR-GBP,EUR-NOK,CAD-USD,CAD-BRL,AUD-USD,USD-JPY,USD-CHF,USD-CAD,NZD-USD,USD-ZAR,USD-TRY,USD-BRL'
-
-  ativosDataSubject = new BehaviorSubject<any>(['Hello World'])
 
   constructor(private http: HttpClient) { }
 
