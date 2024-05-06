2e7cf425ff28c22858a4cb9b1d0659de78b5fc7d
Felipe Lelles Carneiro <contactfelipelc@gmail.com>
2024-05-06 15:07:23 -0300

    creating economic calendar

--- src/app/services/consultas.service.ts
+++ src/app/services/consultas.service.ts
@@ -7,7 +7,8 @@ import { Injectable } from '@angular/core';
 export class ConsultasService {
 
   // apiKey twelvedata
-  apiKey = '13a61d54da3947b6bdc7c92eb9747b8c'
+  apiKeyDataChart = '13a61d54da3947b6bdc7c92eb9747b8c'
+  apiKeyCalendar = 'bf8fdf98812c4db:pob4a3ny4mf02eg'
   
   ativos = 'EUR-BBD,EUR-BDT,EUR-BGN,EUR-BHD,EUR-BIF,EUR-BND,BTC-USD,ETH-USD,LTC-USD,XRP-USD,DOGE-USD,GBP-USD,EUR-CHF,EUR-USD,EUR-CAD,EUR-JPY,EUR-NZD,EUR-AUD,EUR-GBP,EUR-NOK,CAD-USD,CAD-BRL,AUD-USD,USD-JPY,USD-CHF,USD-CAD,NZD-USD,USD-ZAR,USD-TRY,USD-BRL'
 
@@ -18,6 +19,10 @@ export class ConsultasService {
   }
 
   consultaAtivo(ativo: any, timeframe: any) {
-    return this.http.get("https://api.twelvedata.com/time_series?symbol=" + ativo + "&interval=" + timeframe + "&apikey=" + this.apiKey)
+    return this.http.get("https://api.twelvedata.com/time_series?symbol=" + ativo + "&interval=" + timeframe + "&apikey=" + this.apiKeyDataChart)
+  }
+
+  consultaCalendario() {
+    return this.http.get("https://api.tradingeconomics.com/calendar?c=" + this.apiKeyCalendar)
   }
 }
